eec6f03bcf036c49bc5ab9a58a2dd1aaf3072de6
sanderaarts <sanderaarts@users.noreply.github.com>
2015-07-24 22:19:21 +0200

    Added .gitignore

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ ln -s ~/.dot-files/.bashrc ~/.bashrc
 ln -s ~/.dot-files/.inputrc ~/.inputrc
 ln -s ~/.dot-files/.screenrc ~/.screenrc
 ln -s ~/.dot-files/.profile ~/.profile
-
+ln -s ~/.dot-files/.gitignore ~/.gitignore
