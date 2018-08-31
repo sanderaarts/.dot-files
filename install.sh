b937312497918d52560cb55d64060168f83fc517
sanderaarts <github.com@zoid.nl>
2018-08-31 12:21:02 +0200

    Added basic .gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ln -s ~/.dot-files/.bashrc ~/.bashrc
-ln -s ~/.dot-files/.inputrc ~/.inputrc
-ln -s ~/.dot-files/.screenrc ~/.screenrc
-ln -s ~/.dot-files/.profile ~/.profile
-ln -s ~/.dot-files/.gitignore ~/.gitignore
 ln -s ~/.dot-files/.colordiffrc ~/.colordiffrc
+ln -s ~/.dot-files/.gitconfig ~/.gitconfig
+ln -s ~/.dot-files/.gitignore ~/.gitignore
+ln -s ~/.dot-files/.inputrc ~/.inputrc
+ln -s ~/.dot-files/.profile ~/.profile
+ln -s ~/.dot-files/.screenrc ~/.screenrc
