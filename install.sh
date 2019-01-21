61623c3a180a82f45b5f9f80e37abf4431b0c753
sanderaarts <github.com@zoid.nl>
2019-01-21 14:23:44 +0100

    Added .npmrc

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -s ~/.dot-files/.colordiffrc ~/.colordiffrc
 ln -s ~/.dot-files/.gitconfig ~/.gitconfig
 ln -s ~/.dot-files/.gitignore ~/.gitignore
 ln -s ~/.dot-files/.inputrc ~/.inputrc
+ln -s ~/.dot-files/.npmrc ~/.npmrc
 ln -s ~/.dot-files/.profile ~/.profile
 ln -s ~/.dot-files/.screenrc ~/.screenrc
