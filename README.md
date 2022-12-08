716ca395cd13b1509f47e2fdcbd0227900cdeccf
Sander Aarts <sanderaarts@users.noreply.github.com>
2022-12-08 14:20:59 +0100

    Removed install.sh + updated README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dot-files
 Collection of configuration files I rollout when logging onto a server for the first time
+
+## Scripts
+The scripts assume this repo is located at ~/.dot-files.
+* setup-bash
+* setup-zsh
+* setup-git
+* setup-other
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ln -s ~/.dot-files/.bashrc ~/.bashrc
-ln -s ~/.dot-files/.colordiffrc ~/.colordiffrc
-ln -s ~/.dot-files/.gitconfig ~/.gitconfig
-ln -s ~/.dot-files/.gitignore ~/.gitignore
-ln -s ~/.dot-files/.inputrc ~/.inputrc
-ln -s ~/.dot-files/.npmrc ~/.npmrc
-ln -s ~/.dot-files/.profile ~/.profile
-ln -s ~/.dot-files/.screenrc ~/.screenrc
